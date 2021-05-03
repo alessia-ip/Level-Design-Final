a6445384c996adb1dc7c125f56803a8beef8140d
UpbeatBeau <70418283+UpbeatBeau@users.noreply.github.com>
2021-05-03 01:21:46 -0400

    Portal works

---
+++ FinalLevel/Assets/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+  public GameObject exitPortal;
+  public GameObject enterPortal;
+  public CharacterController player;
+
+  private void OnTriggerEnter(Collider other)
+  {
+   
+    if (other.gameObject.tag.Contains("Portal"))
+    {
+      Debug.Log("Yay");
+      player.enabled = false;
+      transform.position = exitPortal.transform.position;
+      Destroy(exitPortal);
+      Destroy(enterPortal);
+      player.enabled = true;
+    }
+  }
+}
